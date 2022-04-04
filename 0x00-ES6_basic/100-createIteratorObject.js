797e8f4c5da07bb9e06000c230caf8d070ccf575
layan2k <lesliedeeshumba@gmail.com>
2022-04-04 17:00:31 +0200

    fixes

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,12 @@
 export default function createIteratorObject(report) {
-  const output = report.allEmployees;
-  let values = [];
-  output.forEach((value) => { values = [...values, ...value]; });
-  return values;
+  const workers = [];
+  /* eslint-disable no-unused-vars */
+  for (const [department, employees] of Object.entries(report.allEmployees)) {
+    for (const employe of employees) {
+      workers.push(employe);
+    }
+  }
+  /* eslint-enable no-unused-vars */
+
+  return workers;
 }
