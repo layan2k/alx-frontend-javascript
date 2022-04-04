9446ce8b678825de99fa9ba3718321099362d4a3
layan2k <lesliedeeshumba@gmail.com>
2022-04-04 16:32:07 +0200

    update func

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -3,7 +3,7 @@ export default function createReportObject(employeesList) {
     allEmployees: {
       ...employeesList,
     },
-    getDepartments(employeesList) {
+    getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
     },
   };
